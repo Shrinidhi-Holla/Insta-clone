abc3faad6ef838ff57347e6471da39773594d658
Shrinidhi Holla <hollashrinidhi99@gmail.com>
2024-06-11 00:18:24 +0530

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:LoginRegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
